2bed25ab687f96482556af5c157f25e59c1113b8
Tiago de França Queiroz <tiago.f.q@gmail.com>
2012-11-13 15:37:06 -0200

    Constructor, replica and apaga implemented

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.rmi.RemoteException;
+import java.util.Hashtable;
+import java.util.TreeSet;
 
 import controler.NenhumServidorDisponivelException;
 import controler.ObjetoNaoEncontradoException;
@@ -13,16 +15,36 @@ import controler.ObjetoNaoEncontradoException;
  */
 public class Server implements InterfaceAcesso, InterfaceReplicacao
 {
+    Hashtable<Integer, Object> h;
+    TreeSet<Integer> bst;
+
+    public Server()
+    {
+        h = new Hashtable<Integer, Object>();
+        bst = new TreeSet<Integer>();
+    }
+
     public void replica(int id, Object obj) throws RemoteException,
     NenhumServidorDisponivelException
     {
-        // TODO Auto-generated method stub
+        Integer i = new Integer(id);
+
+        h.put(i, obj);
+        bst.add(i);
     }
 
     public void apaga(int id) throws RemoteException,
     NenhumServidorDisponivelException, ObjetoNaoEncontradoException
     {
-        // TODO Auto-generated method stub
+        Integer i = new Integer(id);
+
+        if(! bst.contains(i))
+            throw new ObjetoNaoEncontradoException(i.toString());
+        else
+        {
+            bst.remove(i);
+            h.remove(i);
+        }
     }
 
     public Object recupera(String link) throws RemoteException,
