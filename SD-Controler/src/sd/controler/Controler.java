9eadeb11d461249bd0ff0fbf70701e19a50ed41e
Tiago de França Queiroz <tiago.f.q@gmail.com>
2012-11-14 00:26:47 -0200

    Controler calss

---
+++ SD-Controler/src/sd/controler/Controler.java
@@ -0,0 +1,14 @@
+package sd.controler;
+
+/**
+ * 
+ * @author Anderson de França Queiroz <contato (at) andersonq (dot) eti (dot) br
+ * @author André Xavier Martinez
+ * @author Tiago de França Queiroz <contato (at) tiago (dot) eti (dot) br
+ *
+ */
+
+public class Controler
+{
+
+}
