7f9238f9bddec2fe8cad14879a33c67dbc901963
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-16 17:48:05 -0200

    Documentation

--- SD-Interfaces/src/sd/types/Box.java
+++ SD-Interfaces/src/sd/types/Box.java
@@ -2,9 +2,17 @@ package sd.types;
 
 import java.io.Serializable;
 
+/**
+ * A box to store any type of object
+ * @author Anderson Queiroz <contato(at)andersonq(dot)eti(dot)br
+ * @author André Xavier Martinez
+ * @author Tiago de França Queiroz <contato (at) tiago (dot) eti (dot) br
+ * 
+ */
 @SuppressWarnings("serial")
 public class Box implements Serializable
 {
+	/** The stored object */
     Object obj;
 
     /**
