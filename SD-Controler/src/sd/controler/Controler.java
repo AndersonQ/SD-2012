0732b556f7cf3022ad9c236c3d1b1d1b0f1460f7
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-16 16:31:21 -0200

    Methods to override created

--- SD-Controler/src/sd/controler/Controler.java
+++ SD-Controler/src/sd/controler/Controler.java
@@ -2,6 +2,13 @@ package sd.controler;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+import sd.exceptions.NenhumServidorDisponivelException;
+import sd.exceptions.ObjetoNaoEncontradoException;
+import sd.interfaces.InterfaceControlador;
+import sd.interfaces.InterfaceReplicacao;
+import sd.types.Box;
 
 /**
  * 
@@ -12,12 +19,59 @@ import java.rmi.server.UnicastRemoteObject;
  */
 
 @SuppressWarnings("serial")
-public class Controler extends UnicastRemoteObject
+public class Controler extends UnicastRemoteObject implements InterfaceControlador, InterfaceReplicacao
 {
 
     protected Controler() throws RemoteException
     {
         super();
     }
+
+    //=======================Begin of interface Controller=======================
+	@Override
+	public void armazena(String nome, Box obj) throws RemoteException,
+			NenhumServidorDisponivelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String procura(String nome) throws RemoteException,
+			NenhumServidorDisponivelException, ObjetoNaoEncontradoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<String> lista() throws RemoteException,
+			NenhumServidorDisponivelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void apaga(String nome) throws RemoteException,
+			NenhumServidorDisponivelException, ObjetoNaoEncontradoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void apaga(Integer arg0) throws RemoteException,
+			NenhumServidorDisponivelException, ObjetoNaoEncontradoException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//=======================End of interface Controller=======================
+
+	//=======================Begin of interface Replication=======================
+	@Override
+	public void replica(Integer arg0, Box arg1) throws RemoteException,
+			NenhumServidorDisponivelException {
+		// TODO Auto-generated method stub
+		
+	}
+	//=======================End of interface Replication=======================
 
 }
