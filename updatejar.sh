784c9f1304f0e3184a833a487895ba5a206f40ba
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-19 17:42:39 -0200

    Correcting spelling

--- updatejar.sh
+++ updatejar.sh
@@ -2,5 +2,5 @@
 
 cp SD-Interfaces/jar/interfaces.jar SD-Server/jar/ -v
 cp SD-Interfaces/jar/interfaces.jar SD-Client/jar/ -v
-cp SD-Interfaces/jar/interfaces.jar SD-Controler/jar/ -v
+cp SD-Interfaces/jar/interfaces.jar SD-Controller/jar/ -v
 
