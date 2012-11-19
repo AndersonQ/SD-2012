d91193aadd16341320519626ad70d005b8b39eaa
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-19 17:15:28 -0200

    Interface to servers conect to controller

--- SD-Interfaces/src/sd/interfaces/InterfaceControllerServer.java
+++ SD-Interfaces/src/sd/interfaces/InterfaceControllerServer.java
@@ -4,4 +4,14 @@ import java.rmi.Remote;
 
 public interface InterfaceControllerServer extends Remote {
 
+	/**
+	 * Ask to connect to controller sending ServerName 
+	 * to build this tuple {object_id, server, service} with this syntax: 
+	 * ID@rmi://SERVER_NAME/SERVICE_NAME
+	 * the service name is SEVERID, and ID will be returned
+	 * @param name the name of the server 
+	 * @param service name of the service
+	 * @return the ID of this server or -1 if its fail
+	 */
+	int beforeConect(String name);
 }
