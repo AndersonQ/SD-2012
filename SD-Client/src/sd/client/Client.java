0ec9a05d45600344beb0d2f2edbb760a6ae67dd6
André <zeroxm@hotmail.com>
2012-12-01 20:48:06 -0200

    Fiz uma interface para o cliente com as funções que funcionam, vou acertar a lista de servidores de controlador, que está sempre vazia, e sendo assim, não dá para usar funções que peçam o nextSercer()

--- SD-Client/src/sd/client/Client.java
+++ SD-Client/src/sd/client/Client.java
@@ -6,6 +6,9 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.sun.xml.internal.ws.api.pipe.NextAction;
 
 import sd.exceptions.NenhumServidorDisponivelException;
 import sd.exceptions.ObjetoNaoEncontradoException;
@@ -30,7 +33,9 @@ public class Client
         InterfaceAcesso ia_cliente = null;
         InterfaceControlador ic_cliente= null;
         ArrayList<String> ALS = null;
-        String busca=null;
+        String busca="", nome="", conteudo="";
+        Scanner sc = new Scanner(System.in);
+        int opt=1;
         /* Connect to the Controller */
         try
         {
@@ -57,51 +62,87 @@ public class Client
             e.printStackTrace();
         }
         try{
-        System.out.println(ic_cliente.Connection_cliente_OK());
+        System.out.println(ic_cliente.Connection_cliente_OK()+"\n");
         }
         catch (RemoteException e)
         {
             System.out.println("RemoteException");
             e.printStackTrace();
-        }
-        /*
-         * Create objects, box them and
-         * send to server store it.
-         */
-        try
-        {
-        	ic_cliente.armazena("Dez", new Box((Object) new String("Dez")));
-        	ic_cliente.armazena("Vinte", new Box((Object) new String("Vinte")));
-        	ic_cliente.armazena("Trinta", new Box((Object) new String("Trinta")));
-        	ic_cliente.armazena("Quarenta", new Box((Object) new String("Quarenta")));
-        	ALS = ic_cliente.lista();
-        	System.out.println("Listando objetos armazenados");
-        	for (String nome: ALS)
-        		System.out.println(nome);
-        	ic_cliente.intControladorApaga("Dez");
-        	ic_cliente.intControladorApaga("Trinta");
-        	ALS = ic_cliente.lista();
-        	System.out.println("Listando objetos armazenados aós uso de função apagar");
-        	for (String nome: ALS)
-        		System.out.println(nome);
-        	busca=ic_cliente.procura("Vinte");
-        	System.out.println(busca);
-        	busca=ic_cliente.procura("Trinta");        	
-        }
-        catch (RemoteException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (NenhumServidorDisponivelException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ObjetoNaoEncontradoException e) {
-			// TODO Auto-generated catch block
-        	System.out.println("Objeto não encontrado");
-			e.printStackTrace();
-		}
-    }
+        }	while (opt > 0)	{			//Loop infinito enquanto o programa atende o usuário.
+        	/*Dialogo com o usuário*/
+        	System.out.println("O que deseja fazer?\n1 - Criar e armazenar objeto\n2 - Procurar um objeto\n3 - Recuperar um objeto\n4 - Apagar um objeto\n5 - Listar objetos disponíveis");
+        	opt=sc.nextInt();
+        	switch (opt) {
+        	/*Armazenamento de objetos*/
+			case 1:
+	        	System.out.println("Digite o nome do objeto:");
+	        	nome=sc.next();
+	        	System.out.println("Digite o seu conteúdo:");
+	        	conteudo=sc.next();
+	        	System.out.println("Armazenando objeto...");
+	        	try{
+	        	ic_cliente.armazena(nome, new Box((Object) conteudo));
+	        	}
+	        	catch (RemoteException e)
+	            {
+	                 // TODO Auto-generated catch block
+	                 e.printStackTrace();
+	            }
+	            catch (NenhumServidorDisponivelException e)
+	            {
+	                 // TODO Auto-generated catch block
+	                 e.printStackTrace();
+	            }
+	            System.out.println("Objeto armazenado com sucesso\n");
+				break;
+				
+			case 2:
+				
+				break;
 
+			case 3:
+				
+				break;
+				
+			case 4:
+				System.out.println("\nDigite o nome do objeto a ser apagado");
+				nome=sc.next();
+				try {
+					ic_cliente.intControladorApaga(nome);
+					System.out.println("\nProcurando...\n");
+					System.out.println("Objeto removido com sucesso\n");
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NenhumServidorDisponivelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ObjetoNaoEncontradoException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Objeto Não encontrado\n");
+				}
+				
+				break;
+				/*Listar objetos disponíveis*/
+			case 5:
+				try {
+					ALS = ic_cliente.lista();
+				} catch (RemoteException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (NenhumServidorDisponivelException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	        	System.out.println("\nListando nomes dos objetos armazenados");
+	        	for (String nome_objetos: ALS)
+	        		System.out.println(nome_objetos);
+	        		System.out.println();
+				break;
+	
+			default:
+			
+				break;
+			}
+    }}
 }
