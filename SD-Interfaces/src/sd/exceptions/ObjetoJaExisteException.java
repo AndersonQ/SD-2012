08e275ea644f6ecd29fadc29699a405ad38468ec
Tiago de França Queiroz <contato@tiago.eti.br>
2012-12-02 01:01:05 -0200

    new exception

---
+++ SD-Interfaces/src/sd/exceptions/ObjetoJaExisteException.java
@@ -0,0 +1,25 @@
+package sd.exceptions;
+
+/**
+ * 
+ * @author Anderson de França Queiroz <contato (at) andersonq (dot) eti (dot) br
+ * @author André Xavier Martinez
+ * @author Tiago de França Queiroz <contato (at) tiago (dot) eti (dot) br
+ *
+ */
+
+@SuppressWarnings("serial")
+public class ObjetoJaExisteException extends Exception
+{
+
+    /**
+     * Creates an exception with the message
+     * passed as argument
+     * @param message
+     */
+    public ObjetoJaExisteException(String message)
+    {
+        super(message);
+    }
+
+}
